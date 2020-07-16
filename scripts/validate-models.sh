6f128af7798ca2d19f10e89363c7cd09290bf4fb
Matt Jarvis <matt.jarvis@gmail.com>
2020-07-16 08:38:14 +0100

    Add script to run the FHIR validator on all resources of a particular type against a profile from a given implementation guide.

---
+++ scripts/validate-models.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+function validateResources {
+    # Tidy up files from previous runs
+    rm "$1_"*
+    rm "ValidationResults_$1.txt"
+
+    # Extract all resources of the selected type into a file, one per line
+    for f in models/examples/*/*.json
+    do
+        jq -c ".entry[]?.resource? | select(.resourceType==\"$1\")" < "$f" >> "$1_All"
+    done
+
+    # Split each line into a separate file and remove the original file
+    split -l 1 "$1_All" "$1_"
+    rm "$1_All"
+
+    # Validate the individual files against the selected profile
+    java -jar models/dist/org.hl7.fhir.validator.jar "./$1_"* \
+        -version 4.0.1 -tx n/a -ig "$2" -profile "$3" \
+        | tee "ValidationResults_$1.txt"
+
+    # Tidy up temp files
+    rm "$1_"*
+}
+
+wget -nc https://packages.simplifier.net/UK.Core.r4/-/UK.Core.r4-1.2.0.tgz
+wget -nc https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.18-dev.tgz
+
+# validateResources "Patient" "./UK.Core.r4-1.2.0.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/UKCore-Patient"
+validateResources "Patient" "./UK.DM.r4-0.0.18-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient"
+validateResources "MedicationRequest" "./UK.DM.r4-0.0.18-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest"
