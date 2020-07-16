802a3a186bcd5a99a6dcd7c42349a6fc0c827a71
Matt Jarvis <matt.jarvis@gmail.com>
2020-07-16 10:44:58 +0100

    Changed script to download the latest versions of packages rather than a fixed version.

--- scripts/validate-models.sh
+++ scripts/validate-models.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -x
 
+function downloadLatestPackageVersion {
+    manifest=$(curl "https://packages.simplifier.net/$1")
+    version=$(echo "$manifest" | jq -r '[.versions[]][-1].version')
+    url=$(echo "$manifest" | jq -r '[.versions[]][-1].url')
+    filename="./$1-$version.tgz"
+    wget -nc -O "$filename" "$url"
+    echo "$filename"
+}
+
 function validateResources {
     # Tidy up files from previous runs
     rm "$1_"*
@@ -25,14 +34,14 @@ function validateResources {
     rm "$1_"*
 }
 
-wget -nc https://packages.simplifier.net/UK.Spine.r4/-/UK.Spine.r4-0.0.3-dev.tgz
-wget -nc https://packages.simplifier.net/UK.Core.r4/-/UK.Core.r4-1.2.0.tgz
-wget -nc https://packages.simplifier.net/UK.DM.r4/-/UK.DM.r4-0.0.18-dev.tgz
+# ukCorePackage=$(downloadLatestPackageVersion "UK.Core.r4")
+ukSpinePackage=$(downloadLatestPackageVersion "UK.Spine.r4")
+ukDMPackage=$(downloadLatestPackageVersion "UK.DM.r4")
 
-# validateResources "Patient" "./UK.Core.r4-1.2.0.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/UKCore-Patient"
-validateResources "Patient" "./UK.DM.r4-0.0.18-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient"
-validateResources "MedicationRequest" "./UK.DM.r4-0.0.18-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest"
-validateResources "PractitionerRole" "UK.Spine.r4-0.0.3-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-PractitionerRole"
-validateResources "Practitioner" "UK.Spine.r4-0.0.3-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-Practitioner"
-validateResources "Organization" "UK.Spine.r4-0.0.3-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-Organization"
-validateResources "MessageHeader" "UK.Spine.r4-0.0.3-dev.tgz" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-MessageHeader"
+# validateResources "Patient" "$ukCorePackage" "https://fhir.nhs.uk/R4/StructureDefinition/UKCore-Patient"
+validateResources "Patient" "$ukDMPackage" "https://fhir.nhs.uk/R4/StructureDefinition/DM-Patient"
+validateResources "MedicationRequest" "$ukDMPackage" "https://fhir.nhs.uk/R4/StructureDefinition/DM-MedicationRequest"
+validateResources "PractitionerRole" "$ukSpinePackage" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-PractitionerRole"
+validateResources "Practitioner" "$ukSpinePackage" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-Practitioner"
+validateResources "Organization" "$ukSpinePackage" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-Organization"
+validateResources "MessageHeader" "$ukSpinePackage" "https://fhir.nhs.uk/R4/StructureDefinition/Spine-MessageHeader"
